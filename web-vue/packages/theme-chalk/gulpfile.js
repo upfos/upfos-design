9fc03ff4ba9a22199abe5174286a313b249e38c4
chrissie <shengjiaoqing@163.com>
2022-03-15 16:57:48 +0800

    other：修改样式打包配置

--- web-vue/packages/theme-chalk/gulpfile.js
+++ web-vue/packages/theme-chalk/gulpfile.js
@@ -16,4 +16,8 @@ function copyfont(){ // 拷贝字体样式
     return src('./src/fonts/**').pipe(cssmin()).pipe(dest('./lib/fonts'))
 }
 
-exports.build = series(compile,copyfont)
+function copystyle() { // 拷贝index样式到design
+    return src('./lib/index.css').pipe(dest('../design/lib'))
+}
+
+exports.build = series(compile,copyfont,copystyle)
